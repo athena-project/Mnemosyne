a620320634408e95fc282d2c0182002f98c82619
PROSPERI Laurent <oceane21.sk1@live.fr>
2014-12-07 20:10:19 +0100

    0.2.0

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,9 @@
-﻿#! /bin/bash
+﻿#!/bin/bash
+
+CURRENT_DIR=`dirname $0` #Dossier parent du script d'installation
+
+
+
 
 ###
 ### BEGIN DEPENDENCES
@@ -8,16 +13,16 @@
 mkdir /usr/opt
 apt-get update
 apt-get upgrade
-apt-get install build-essential g++ cmake make liblzma-dev libtar-dev python3-dev
+apt-get install -y build-essential g++ cmake make liblzma-dev libtar-dev python3-dev
 
 ##MySQL
-apt-get install mysql-server
-mysql -u root -p"rj7@kAv;8d7_e(E6:m4-w&" -e "CREATE DATABASE mnemosyne"
-mysql -u root -p"rj7@kAv;8d7_e(E6:m4-w&" -D mnemosyne -e "SOURCE tables.sql"
+#apt-get install mysql-server
+#mysql -u root -p"rj7@kAv;8d7_e(E6:m4-w&" -e "CREATE DATABASE mnemosyne"
+#mysql -u root -p"rj7@kAv;8d7_e(E6:m4-w&" -D mnemosyne -e "SOURCE tables.sql"
 #config ?
 
 ##MySQL++
-apt-get install libmysqlclient-dev
+apt-get install -y libmysqlclient-dev
 mkdir -p /usr/local/mysql/lib
 ln -s /usr/lib/x86_64-linux-gnu/libmysqlclient.so   /usr/local/mysql/lib/libmysqlclient.so  #petit pb de lib
 	
@@ -33,12 +38,12 @@ rm -r mysql++-3.2.1
 rm mysql++-3.2.1.tar.gz
 
 ### Boost for python3.*
-apt-get install libboost-all-dev
+apt-get install -y libboost-all-dev
 wget http://sourceforge.net/projects/boost/files/boost/1.56.0/boost_1_56_0.tar.gz/download -O boost_1_56_0.tar.gz
 tar xvfz boost_1_56_0.tar.gz
 cd boost_1_56_0
 sh bootstrap.sh --prefix=/usr/opt --with-libraries=python --with-python-version=3.4
-sh b2
+./b2
 
 mkdir /usr/opt/boost_1_56_0
 cp stage/lib/* /usr/opt/boost_1_56_0/
@@ -56,7 +61,7 @@ rm boost_1_56_0.tar.gz
 ###
 mkdir /usr/opt/mnemosyne
 
-cd ../build
+cd $CURRENT_DIR/../build
 cmake .
 make 
 cp libpyRessource.so /usr/opt/mnemosyne
--- install/table.sql
+++ install/table.sql
@@ -7,7 +7,6 @@ DROP TABLE IF EXISTS `chunk`;
 CREATE TABLE `chunk` (
   `id` BIGINT(11) NOT NULL AUTO_INCREMENT,
   `size` INT(11),
-  PRIMARY KEY (`id`),
-  KEY key_block_id (block_id)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
