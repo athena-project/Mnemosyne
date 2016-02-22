cb44be6ebd930270a215a4663b3b772c20d7c825
severus21 <laurent.prosperi658@orange.fr>
2016-02-22 16:07:42 +0100

    [network] Fix : TCPHandler

--- src/network/TCPHandler.cpp
+++ src/network/TCPHandler.cpp
@@ -1,4 +1,4 @@
-#include "TCPClient.h"
+#include "TCPHandler.h"
 
 int Handler::add_to_in(char* buf, int size){
 	if( in_offset+size > in_length){
@@ -22,41 +22,40 @@ int Handler::out_write(){
 
 
 ///TCPServer
-
-TCPServer::TCPServer(char* port, int _pfd){
+TCPServer::TCPServer(char* port, int _pfd, std::atomic<bool>* _alive, 
+list<Task>*_tasks, mutex* _m_tasks){
 	pfd= _pfd;
 	struct epoll_event event;
-
+	alive = _alive;
+	tasks = _tasks; 
+	m_tasks = _m_tasks; 
+	
 	sfd = create_and_bind (port);
 	if(sfd == -1)
 		abort();
 
-	s = make_socket_non_blocking (sfd);
+	s = make_non_blocking (sfd);
 	if (s == -1)
 		abort();
 
 	s = listen (sfd, SOMAXCONN);
 	if(s == -1){
-		perror ("listen");
-		abort ();
+		perror("listen");
+		abort();
 	}
 
 	efd = epoll_create1(0);
 	if(efd == -1){
-		perror ("epoll_create");
-		abort ();
+		perror("epoll_create-TCPServer");
+		abort();
 	}
 
 	event.data.fd = sfd;
 	event.events = EPOLLIN | EPOLLET;
 	s = epoll_ctl (efd, EPOLL_CTL_ADD, sfd, &event);
-	
-	event.data.fd = pfd; //to communicate with warper
-	event.events = EPOLLIN | EPOLLET;
-	s = epoll_ctl (efd, EPOLL_CTL_ADD, sfd, &event);
 	if(s == -1){
-		perror ("epoll_ctl");
-		abort ();
+		perror("epoll_ctl-TCPServer2");
+		abort();
 	}
 
 	/* Buffer where events are returned */	
@@ -80,35 +79,33 @@ int TCPServer::create_and_bind (char *port){
 	}
 
 	for(rp = result; rp != NULL; rp = rp->ai_next){
-		sfd = socket (rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+		sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
 		if (sfd == -1)
 		continue;
 
-		s = bind (sfd, rp->ai_addr, rp->ai_addrlen);
-		if (s == 0){
-			/* We managed to bind successfully! */
+		s = bind(sfd, rp->ai_addr, rp->ai_addrlen);
+		if (s == 0)
 			break;
-		}
 
 		close (sfd);
 	}
 
 	if (rp == NULL){
-		fprintf (stderr, "Could not bind\n");
+		fprintf(stderr, "Could not bind\n");
 		return -1;
 	}
 
-	freeaddrinfo (result);
+	freeaddrinfo(result);
 
 	return sfd;
 }
 
-int TCPServer::make_socket_non_blocking (int sfd){
+int TCPServer::make_non_blocking (int sfd){
   int flags, s;
 
-	flags = fcntl (sfd, F_GETFL, 0);
-	if (flags == -1){
-		perror ("fcntl");
+	flags = fcntl(sfd, F_GETFL, 0);
+	if(flags == -1){
+		perror("fcntl");
 		return -1;
 	}
 
@@ -122,51 +119,53 @@ int TCPServer::make_socket_non_blocking (int sfd){
   return 0;
 }
 
-void TCPServer::pcallback(Handler& handler, msg_t type){
-	struct epoll_event event;
-	event.events = EPOLLIN | EPOLLET;
-	s = epoll_ctl (efd, EPOLL_CTL_MOD, pfd, &event);
-	if(s == -1){
-		perror("epoll_ctl");
-		abort();
-	}
-    
-    char* end = handler.get_in_data() + sizeof(int);    
-    int port = strtol(handler.get_in_data(), &end, 0);
-    end = handler.get_in_data() + sizeof(int) + sizeof(size_t);
-    int host_len = strtol(handler.get_in_data()+sizeof(int), &end, 0);
-    
-	int sockfd;
-	struct sockaddr_in addr;
-    /* socket: create the socket */
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        perror("ERROR opening socket");
-
-    addr.sin_family = AF_INET;	
-	char tmp[host_len];	
-	strcpy(tmp, handler.get_in_data() + sizeof(int)+sizeof(size_t));
-	addr.sin_addr.s_addr = inet_addr( tmp );	
-    addr.sin_port = htons(port);
-
-	
-    if(connect(sockfd, (sockaddr*)&addr, sizeof(addr)) < 0) 
-		perror("ERROR connecting");
-	else{
+void TCPServer::pcallback(){
+	Task t;
+	m_tasks->lock();
+    for(int i=0; i<tasks->size(); i++){
+		t = tasks->front();
+		tasks->pop_front();
+			
+		///Create sending socket
+		int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+		struct sockaddr_in addr;
+		if (sockfd < 0){
+			perror("ERROR opening socket");
+			continue;
+		}
+		
+		addr.sin_family = AF_INET;	
+		inet_pton(AF_INET, t.host, &addr.sin_addr);
+		addr.sin_port = htons(t.port);
+		
+		if(connect(sockfd, (sockaddr*)&addr, sizeof(addr)) < 0){
+			perror("ERROR connecting");
+			continue;
+		}
+		
+		if( make_non_blocking(sockfd) == -1 ){
+			perror("ERROR pcallback");
+			continue;
+		}
+		
 		Handler* n_handler = new Handler(sockfd);
-		n_handler->set_out_data( handler.get_in_data() + sizeof(int) + sizeof(size_t) + host_len);
 		
-		
+		n_handler->set_out_data( (t.msg)->serialize() );
+		n_handler->set_out_length( (t.msg)->s_length() );
+
 		struct epoll_event out_event;
-		out_event.events = EPOLLOUT | EPOLLET;
+		out_event.events = EPOLLOUT;
+		out_event.data.ptr=static_cast<void*>( n_handler );
 		s = epoll_ctl(efd, EPOLL_CTL_ADD, sockfd, &out_event);
+				
 		if(s == -1){
-			perror("epoll_ctl");
+			perror("epoll_ctl-pcallback2");
 			close( n_handler->get_fd() );
 			free( n_handler );
 			abort();
 		}
-	}	
+	}
+	m_tasks->unlock();
 }
 
 void TCPServer::wcallback(Handler* handler, msg_t type){
@@ -174,17 +173,16 @@ void TCPServer::wcallback(Handler* handler, msg_t type){
 	free( handler );
 }
 
-void TCPServer::rcallback(Handler* handler, msg_t type){
-	//Msg msg = handler->getMsg();
-	
+void TCPServer::rcallback(Handler* handler, msg_t type){	
 	struct epoll_event listen_event;
 	listen_event.events = EPOLLOUT | EPOLLET;	
 	listen_event.data.ptr=static_cast<void*>( handler );
+	printf("Msg incomming : |");
 	write (1, handler->get_in_data(), handler->get_in_offset()); //Debug
 
 	s = epoll_ctl (efd, EPOLL_CTL_MOD, handler->get_fd(), &listen_event);
 	if(s == -1){
-		perror("epoll_ctl");
+		perror("epoll_ctl-rcallback");
 		close( handler->get_fd() );
 		free( handler );
 		abort();
@@ -193,8 +191,16 @@ void TCPServer::rcallback(Handler* handler, msg_t type){
 }
 	
 int TCPServer::run(){
-	/* The event loop */
-	while(1){
+	struct epoll_event send_event;
+	send_event.data.fd= pfd;
+	send_event.events = EPOLLIN;
+	s = epoll_ctl (efd, EPOLL_CTL_ADD, pfd, &send_event);
+	if(s == -1){
+		perror("epoll_ctl-TCPServer3");
+		abort();
+	}
+	
+	while( alive->load(std::memory_order_relaxed) ){
 		int n, i;
 
 		n = epoll_wait(efd, events, MAXEVENTS, -1);
@@ -209,45 +215,11 @@ int TCPServer::run(){
 				close (events[i].data.fd);
 				
 				continue;
-			}else if(pfd == events[i].data.fd){
-				Handler handler = Handler( pfd );
-				int done = 0;
-				
-				while (1){
-					ssize_t count;
-					char buf[512];
-					count = read(pfd, buf, sizeof buf);
-					
-					if(count == -1){
-						if(errno != EAGAIN){//All data read
-							perror("read");
-							done = 1;
-						}
-						break;
-					}else if(count == 0){//End-Of-File, connection closed
-						done = 1;
-						break;
-					}
-					
-					s = handler.add_to_in(buf, count);
-					if(s == -1){
-						perror("write");
-						abort();
-					}
-				}
-
-				if(done)
-					pcallback( handler, Msg::get_type( handler.get_in_data() ) );
-				
-				struct epoll_event listen_event;
-				listen_event.events = EPOLLIN | EPOLLET;	
-
-				s = epoll_ctl (efd, EPOLL_CTL_MOD, pfd, &listen_event);
-				if(s == -1){
-					perror("epoll_ctl, read pipe");
-					close( pfd );
-					abort();
-				}
+			}else if( pfd == events[i].data.fd){
+				char buf[32];
+				printf("sending\n");
+				while( read(pfd, buf, sizeof buf) == sizeof(buf) ){}
+				pcallback();
 				
 			}else if(sfd == events[i].data.fd){//One or more connection
 				while (1){
@@ -278,7 +250,7 @@ int TCPServer::run(){
 								"(host=%s, port=%s)\n", infd, hbuf, sbuf);
 					}
 
-					s = make_socket_non_blocking (infd);
+					s = make_non_blocking (infd);
 					if(s == -1)
 						abort();
 
@@ -288,7 +260,7 @@ int TCPServer::run(){
 					
 					s = epoll_ctl (efd, EPOLL_CTL_ADD, infd, &listen_event);
 					if(s == -1){
-						perror("epoll_ctl");
+						perror("epoll_ctl-sfd");
 						free(events[i].data.ptr);
 						abort();
 					}
@@ -300,7 +272,7 @@ int TCPServer::run(){
 
 				while (1){
 					ssize_t count;
-					char buf[512];
+					char buf[BUFF_SIZE];
 					count = read(handler->get_fd(), buf, sizeof buf);
 					if(count == -1){
 						if(errno != EAGAIN){//All data read
@@ -325,7 +297,7 @@ int TCPServer::run(){
 			}else if(events[i].events & EPOLLOUT){
 				int ret;				
 				Handler* handler = static_cast<Handler*>(events[i].data.ptr);
-
+				printf("begin writting\n");
 				ret = handler->out_write();
 	   
 				if( (-1 == ret && EINTR == errno) || 
@@ -337,7 +309,7 @@ int TCPServer::run(){
 					
 					s = epoll_ctl (efd, EPOLL_CTL_ADD, handler->get_fd(), &listen_event);
 					if(s == -1){
-						perror("epoll_ctl");
+						perror("epoll_ctl-out");
 						close( handler->get_fd() );
 						free( handler );
 						abort();
@@ -358,13 +330,22 @@ int TCPServer::run(){
 					wcallback( handler, Msg::get_type( handler->get_out_data() ) );
 				}
 	
+			}else{
+				printf("it is an other event\n");
 			}
 		}
 	}
 }
 
-int main(){
-	//TCPHandler t("1597");
-	//t.send("couco");	
-}
 
+//int main(){
+	//TCPHandler t("1599");
+	//struct timespec req, rem;
+	//req.tv_sec  = 0;
+	//req.tv_nsec = 11999999;
+	//while(1){  
+		//Msg m(I, "coucou", strlen("coucou"));
+		//t.send(m, "127.0.1.1", 1597);
+		//nanosleep(&req, &rem);
+	//}
+//}
---
+++ src/network/TCPHandler.h
@@ -0,0 +1,243 @@
+#ifndef MNEMOSYNE_NETWORK_TCPCLIENT_H
+#define MNEMOSYNE_NETWORK_TCPCLIENT_H
+
+#define MAXEVENTS 64
+#define BUFF_SIZE 1024
+#define MAX_SIZE_IN 65536
+
+#define uint64_s sizeof(uint64_t)
+#define int_s sizeof(int)
+#define size_s sizeof(size_t)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <arpa/inet.h>
+
+#include <thread>
+#include <atomic> 
+#include <mutex> 
+#include <list> 
+#include <time.h>
+
+using namespace std;
+
+enum msg_t{A,I};
+
+class Msg{
+	protected:
+		msg_t type;
+		
+		char* data = NULL;
+		bool data_owner = false;
+		
+		char* s_data = NULL;
+		
+		size_t length = 0;
+		
+	public:
+		Msg(msg_t t, char* d, size_t l) : type(t), data(d), length(l){
+			
+		}
+		
+		Msg(char* _s_data, size_t size){
+			data_owner = true;
+			
+			char* end = _s_data + uint64_s;
+			length = strtoull(_s_data, &end, 0);
+			
+			end += uint64_s;
+			type = static_cast<msg_t>(strtoull(_s_data+uint64_s, &end, 0));
+
+			data = new char[length];
+			memcpy(data, _s_data + 2*uint64_s, length);
+		}
+		
+		~Msg(){
+			if( data != NULL && data_owner)
+				delete[] data;
+			printf("MSg died\n");
+			if( s_data != NULL)
+				delete[] s_data;
+		}
+		
+		size_t s_length(){ return length + 2 * uint64_s; }
+		
+		static msg_t get_type(char* data){
+			char* end = data + 2*uint64_s;
+			return static_cast<msg_t>(strtoull(data+uint64_s, &end, 0));
+		}
+		
+		char* serialize(){
+			if( s_data != NULL)
+				return s_data;
+				
+			s_data = new char[ length + uint64_s * 2 ];
+
+			sprintf(s_data, "%" PRIu64 "", length);
+			sprintf(s_data+uint64_s, "%" PRIu64 "", static_cast<uint64_t>(type));
+			memcpy( s_data+uint64_s * 2, data, length );
+			printf("end");
+			return s_data;
+		}
+};
+
+class Task{
+	public:
+		Msg* msg = NULL;
+		const char* host;
+		int port;
+		
+		Task(){}
+		Task(Msg* _m, const char* _a, int _p) : msg(_m), host(_a), port(_p){}
+};
+
+class Handler{
+	protected:
+		int	fd;
+		
+		char* in_data = NULL;
+		uint32_t in_offset = 0 ;
+		uint32_t in_length = BUFF_SIZE ;
+		
+		char* out_data = NULL;
+		uint32_t out_offset = 0;
+		uint32_t out_length = 0;
+		
+	public:
+		Handler(int _fd) : fd(_fd){
+			in_data = new char[in_length];
+		} 
+		
+		~Handler(){ 
+			if( in_data != NULL)
+				delete[] in_data; 
+		}
+		
+		int get_fd(){ return fd; }
+		char* get_in_data(){ return in_data; }
+		uint32_t get_in_length(){ return in_length; }
+		uint32_t get_in_offset(){ return in_offset; }
+		
+		char* get_out_data(){ return in_data; }
+		void set_out_data(char* d){ out_data = d; }
+		void set_out_length(size_t s){ out_length = s; }
+		uint32_t get_out_length(){ return out_length; }
+		uint32_t get_out_offset(){ return out_offset; }
+
+		void decr_out_length( uint32_t ret ){ out_length-= ret; }
+		void incr_out_offset( uint32_t ret ){ out_offset+= ret; }  
+		
+		int add_to_in(char* buf, int size);
+		
+		int out_write();
+};
+
+
+
+class TCPServer{
+	protected :
+		int efd;
+		int sfd;
+		int s;
+		
+		int pfd;
+		
+		struct epoll_event* events = NULL;
+	
+		atomic<bool>* alive;
+		list<Task>* tasks;
+		mutex* m_tasks;
+	public :
+		TCPServer(){}
+	
+		TCPServer(char* port, int _pfd, std::atomic<bool>* _alive, 
+		list<Task>* _tasks, mutex* _m_tasks);
+		
+		~TCPServer(){
+			if( events == NULL )
+				delete[] events;
+			printf("dead\n");
+			close (sfd);
+			close( pfd);
+		}
+	
+		int create_and_bind (char *port);
+
+		int make_non_blocking (int sfd);
+		
+		//called when new data to send
+		void pcallback();
+		
+		//called when finished to read
+		void rcallback(Handler* handler, msg_t type);
+		
+		//called when finished to write
+		void wcallback(Handler* handler, msg_t type);
+	
+		int run();
+		
+		void operator()(){
+			run();
+		}
+};
+
+void f(TCPServer* t){ (*t)(); }
+
+class TCPHandler{
+	protected:
+		int pfds[2];
+		thread t_server;
+		TCPServer* server = NULL;
+		atomic<bool> alive;
+		
+		list<Task> tasks;
+		mutex m_tasks;
+		
+	public:
+		TCPHandler(char* port){
+			pipe(pfds);
+			alive.store(true,std::memory_order_relaxed); 
+			server = new TCPServer(port, pfds[0], &alive, &tasks, &m_tasks);
+			t_server = std::thread( f,server );
+			t_server.detach();
+		}
+		
+		~TCPHandler(){
+			printf("end sender\n");
+			close( pfds[1]);
+			alive.store(false,std::memory_order_relaxed); 
+			
+			if( server != NULL)
+				delete server;
+			else
+				close( pfds[0] );
+		}
+		
+		void send(Msg& msg, const char* _host, int port){
+			//char* host = inet_ntoa( _host );
+			//size_t len_host = strlen(host);
+			//char buff[ msg.s_length() + 2*int_s + len_host ];
+			//int offset = int_s;
+			
+			//sprintf(buff, "%d", port);
+			//sprintf(buff + offset, "%lu",  len_host); //permet de passer facilement de l'ipv6 à ipv4
+			//memcpy(buff + (offset+=size_s), host, len_host );
+			//memcpy(buff + (offset+=len_host), msg.serialize(),  msg.s_length() );
+			m_tasks.lock();
+			tasks.push_back( Task(&msg, _host, port) );
+			m_tasks.unlock();
+			
+			write(pfds[1], "0", 1);		
+			printf("send\n");	
+		}
+};
+#endif
